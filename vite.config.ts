8e31c4e1837bd4db48667d0e0d919cd568f0b7f1
hiba <>
2025-02-21 12:45:06 +0100

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+// Determine base URL dynamically
+const repoName = '/age_calculator/'; // Replace with your actual repository name
+// const base2 = import.meta.env.MODE === 'production' ? '/repo-name/' : '/';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),   tailwindcss(),],
+  plugins: [react(),   tailwindcss()],
+  base:repoName, 
 })
